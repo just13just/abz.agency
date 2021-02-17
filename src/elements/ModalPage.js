7671318accc98ce1be0ab1ccb96b5f3c56085948
just13just <bkryzhniy@gmail.com>
2021-02-17 20:09:38 +0200

    refactor: del redundant code

--- src/elements/ModalPage.js
+++ src/elements/ModalPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
 const ModalPage = ({ modal, setModal }) => {
