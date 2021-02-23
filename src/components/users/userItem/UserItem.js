14600fe31a0a8ca5223be5bf50f4004223233afa
just13just <bkryzhniy@gmail.com>
2021-02-23 21:54:45 +0200

    style: add overfrlw ability to user mame and user email

--- src/components/users/userItem/UserItem.js
+++ src/components/users/userItem/UserItem.js
@@ -1,16 +1,44 @@
 import React from "react"
 import style from "./UserItem.module.scss";
+import photoCover from './assets/photo-cover.svg'
 
+
+const brokenLink = 'https://frontend-test-assignment-api.abz.agency/images/placeholders/placeholder.png'
 
 const UserItem = ({ item }) => {
-    
+
+    const checkImgSrc = (src, alt) => {
+        const img = new Image()
+        img.src = src
+        img.onload = () => {
+            return src
+        }
+        img.onerror = () => {
+            return alt
+        }
+    }
+
+    const isLongWord = string => (/\w{20,}/gm.test(string)) ? 'item__name_cutting' : null
+
     return (
-        <div className={style.item}>
-            <div className={style.item__avatar}><img src={item.photo} /></div>
-            <div className={style.item__name}>{item.name}</div> {/* cut */}
-            <div className={style.item__position}>{item.position}</div>
-            <div className={style.item__email}></div> {/* cut */}
-            <div className={style.item__phone}>{item.phone}</div>
+        <div className={style.itemWrapper}>
+            <div className={style.item}>
+                <div className={style.item__avatar}><img src={item.photo} /></div>
+                <div className={style.item__name}>
+                    <div className={style[isLongWord(item.name)]}>
+                        {item.name}
+                    </div>
+                </div>
+                <div className={style.item__position}>{item.position}</div>
+                <div className={style.emailWrapper}>
+                    <div className={style.item__email}>
+                        {item.email}
+                    </div>
+                    <span className={style.emailWrapper__tooltip}>{item.email}</span>
+                </div>
+
+                <div className={style.item__phone}>{item.phone}</div>
+            </div>
         </div>
     )
 }
