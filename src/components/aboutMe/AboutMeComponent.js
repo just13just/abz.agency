9e7f6e1a655777dc3bd34984aeaa9b66a70b0396
just13just <bkryzhniy@gmail.com>
2021-02-28 19:44:56 +0200

    refactor: make PixelPerfect desktop version aboutMe component

--- src/components/aboutMe/AboutMeComponent.js
+++ src/components/aboutMe/AboutMeComponent.js
@@ -1,30 +1,28 @@
 import React from "react"
-import style from './AboutMe.module.scss'
-import { Button } from 'reactstrap';
 import { Link } from "react-scroll";
 
 const AboutMeComponent = () => {
 
     return (
-        <div className={style.aboutMe}>
-            <div className={`${style.aboutMe__bg} container`}>
-                <div className={style.content}>
+        <section className='about-me'>
+            <div className='about-me__bg content-container'>
+                <div className='about-me__container'>
                     <div>
-                        <div className={style.content__title}>Test assignment for frontend developer position</div>
-                        <div className={style.content__text}>
+                        <div className='about-me__title'>Test assignment for frontend developer position</div>
+                        <div className='about-me__description'>
                             We kindly remind you that your test assignment should be submitted as a link to github/bitbucket repository.
-                            Please be patient, we consider and repository to every application that meets minimum requirements.
+                            Please be patient, we consider and respond to every application that meets minimum requirements.
                             We look forward to your submission. Good luck! The photo has to scale in the banner area on the different screens
                         </div>
-                        <div className={style.content__button}>
-                            <Button color="danger">
-                                <Link to="formComponent" spy={true} smooth={true}>Sign up now</Link>
-                            </Button>
+                        <div className='about-me__button-wrap'>
+                            <button className='sing-up-btn btn-danger btn'>
+                                <Link to="formComponent" spy={true} smooth={true}><span className='btn-sing-up__text'>Sing up now</span></Link>
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
