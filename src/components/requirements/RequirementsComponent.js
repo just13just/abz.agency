03a2c7496889b63920dc9cf1d0fc83f71e40c8db
just13just <bkryzhniy@gmail.com>
2021-03-02 00:37:46 +0200

    style: make PixelPerfect desktop version requirements component

--- src/components/requirements/RequirementsComponent.js
+++ src/components/requirements/RequirementsComponent.js
@@ -1,38 +1,37 @@
 import React from "react"
 import { Link } from "react-scroll"
-import style from './Requirements.module.scss'
 import manLaptop from "./assets/man-laptop-v1.svg"
 
 const RequirementsComponent = () => {
 
     return (
-        <div className={`${style.requirements} container`}>
+        <section className='requirements content-container'>
             <div>
-                <div className={style.requirements__title}>
-                    Let get acquainted
+                <div className='title'>
+                    Let's get acquainted
                 </div>
-                <div className={style.requirements__contentContainer}>
-                    <div className={style.content}>
-                        <div className={style.content__imgContainer}>
-                            <img src={manLaptop} />
+                <div className='requirements__content-container'>
+                    <div className='requirements__first-column'>
+                        <div className='requirements__img-container'>
+                            <img className='requirements__img-container_img-size' src={manLaptop} />
                         </div>
-                        <div className={`${style.content__text} ${style.text}`}>
-                            <div className={style.text__title}>I am cool frontend developer</div>
-                            <div className={style.text__article}>
-                                We will evaluate how clean your approach to writing CSS and Javascript code is.
-                                You can use any CSS and Javascript 3rd party libraries without any restriction.
+                    </div>
+                    <div className='requirements__second-column text'>
+                        <div className='text__description'>I am cool frontend developer</div>
+                        <div className='text__article'>
+                            We will evaluate how clean your approach to writing CSS and Javascript code is.
+                            You can use any CSS and Javascript 3rd party libraries without any restriction.
                             </div>
-                            <div className={style.text__article}>
-                                If 3rd party css/javascript libraries are added to the project via bower/npm/yarn
-                                you will get bonus points. If you use any task runner (gulp/webpack) you will get bonus points as well.
-                                Slice service directory page PSD mockup into HTML5/CSS3.
+                        <div className='text__article'>
+                            If 3rd party css/javascript libraries are added to the project via bower/npm/yarn
+                            you will get bonus points. If you use any task runner (gulp/webpack) you will get bonus points as well.
+                            Slice service directory page PSD mockup into HTML5/CSS3.
                             </div>
-                            <div className={style.text__btn}><Link to="formComponent" spy={true} smooth={true}>Sing up now</Link></div>
-                        </div>
+                        <div className='text__btn'><Link to="formComponent" spy={true} smooth={true}>Sing up now</Link></div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
