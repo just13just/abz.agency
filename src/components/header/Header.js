516a37d7131d914050d2232aa1ae886ac11b6a6f
just13just <bkryzhniy@gmail.com>
2021-03-10 01:05:57 +0200

    feat: add something looks like burger menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import logo from "./assets/logo.svg";
 import { Link } from 'react-scroll';
 
 const Header = () => {
 
+    const [open, setOpen] = useState(false)
+
     return (
         <header className='header'>
-            <div className='header__container content-container'>
-                <div className='header__logo'>
-                    <Link to="formComponent" spy={true} smooth={true}><img src={logo} /></Link>
-                </div>
+            <div className='header__body content-container'>
+                <Link className='header__logo' to="formComponent" spy={true} smooth={true}>
+                    <img src={logo} alt='logo..' />
+                </Link>
 
-                <nav className='menu__body'>
-                    <ul className='menu__list'>
-                        <li><button className='menu__link btn'>About me</button></li>
-                        <li><button className='menu__link btn'>Relationships</button></li>
-                        <li><button className='menu__link btn'>Requirements</button></li>
-                        <li><button className='menu__link btn'>Users</button></li>
+                <div
+                    className={`header__burger ${open ? 'active' : ''}`}
+                    onClick={() => setOpen(!open)}>
+                    <span></span>
+                </div>
+                <nav className={`header__menu ${open ? 'active' : ''}`}>
+                    <ul className='header__list'>
+                        <li><a href='#' className='header__link'>About me</a></li>
+                        <li><a href='#' className='header__link'>Relationships</a></li>
+                        <li><a href='#' className='header__link'>Requirements</a></li>
+                        <li><a href='#' className='header__link'>Users</a></li>
                         <li>
-                            <button color="link" className='menu__link btn'>
-                                <Link to="formComponent" spy={true} smooth={true}>Sign up</Link>
-                            </button>
+                            <Link
+                                className='header__link'
+                                to="formComponent"
+                                spy={true}
+                                smooth={true}>Sign up</Link>
                         </li>
                     </ul>
                 </nav>
