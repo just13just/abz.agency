80cd929e3ccca2b1164ba3bd754e06f1f1950be9
just13just <bkryzhniy@gmail.com>
2021-03-11 20:13:45 +0200

    style: make mobile media query for abourMe component

--- src/components/aboutMe/AboutMeComponent.js
+++ src/components/aboutMe/AboutMeComponent.js
@@ -11,8 +11,9 @@ const AboutMeComponent = () => {
                         <div className='about-me__title'>Test assignment for frontend developer position</div>
                         <div className='about-me__description'>
                             We kindly remind you that your test assignment should be submitted as a link to github/bitbucket repository.
-                            Please be patient, we consider and respond to every application that meets minimum requirements.
-                            We look forward to your submission. Good luck! The photo has to scale in the banner area on the different screens
+                            <div className='about-me__description_hide'>
+                                Please be patient, we consider and respond to every application that meets minimum requirements.
+                                We look forward to your submission. Good luck! The photo has to scale in the banner area on the different screens</div>
                         </div>
                         <div className='about-me__button-wrap'>
                             <button className='btn-sing-up btn-danger btn btn-reset'>
