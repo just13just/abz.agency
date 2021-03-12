81518db8aaba44e788fb4e31cddc2d92b085158f
just13just <bkryzhniy@gmail.com>
2021-03-12 21:43:22 +0200

    style: make content behind open burger menu unscrollable and in shade

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import logo from "./assets/logo.svg";
 import { Link } from 'react-scroll';
 
 const Header = () => {
 
     const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        open
+            ? document.body.style.overflow = "hidden"
+            : document.body.style.overflow = "scroll"
+    }, [open])
 
     return (
         <header className='header'>
@@ -19,33 +25,35 @@ const Header = () => {
                     <span></span>
                 </div>
                 <nav className={`header__menu ${open ? 'active' : ''}`}>
-                    <ul className='header__list'>
-                        <li>
-                            <div className='header__link'>
-                                <Link to='' spy={true} smooth={true}>About me</Link>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='header__link'>
-                                <Link to='' spy={true} smooth={true}>Relationships</Link>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='header__link'>
-                                <Link to='' spy={true} smooth={true}>Requirements</Link>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='header__link'>
-                                <Link to='' spy={true} smooth={true}>Users</Link>
-                            </div>
-                        </li>
-                        <li>
-                            <div className='header__link'>
-                                <Link to='formComponent' spy={true} smooth={true}>Sign up</Link>
-                            </div>
-                        </li>
-                    </ul>
+                    <div className='header__menu-container'>
+                        <ul className='header__list'>
+                            <li>
+                                <div className='header__link'>
+                                    <Link to='' spy={true} smooth={true}>About me</Link>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='header__link'>
+                                    <Link to='' spy={true} smooth={true}>Relationships</Link>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='header__link'>
+                                    <Link to='' spy={true} smooth={true}>Requirements</Link>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='header__link'>
+                                    <Link to='' spy={true} smooth={true}>Users</Link>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='header__link'>
+                                    <Link to='formComponent' spy={true} smooth={true}>Sign up</Link>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </header>
